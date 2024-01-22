725739028fedd9c8367e36a10d5c1c8e7e2ac324
THIRI AUNG <ss.sonali2003@gmail.com>
2024-01-22 21:57:13 +0630

    nabar[develpoing]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
+import { BrowserRouter } from "react-router-dom";
+// import {
+//   Navbar,
+//   Hero,
+//   About,
+//   Experience,
+//   Tech,
+//   Works,
+//   Feedbacks,
+//   Contact,
+//   StarsCanvas
+// } from './components';
+import Navbar from "./components/navbar.jsx";
+import Hero from "./components/hero.jsx";
+import About from "./components/About.jsx";
+import Experience from "./components/Experience.jsx";
+import Tech from "./components/tech.jsx";
+import Works from "./components/works.jsx";
+import Feedbacks from "./components/feedback.jsx";
 
-import './App.css'
+import Contact from "./components/contact.jsx";
+import StarsCanvas from "./components/starsCanvas.jsx";
 
-function App() {
- 
+import "./App.css";
 
+const App = () => {
   return (
-    <div className="">
-      3D Developer
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        {/* //header */}
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        {/* body */}
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
